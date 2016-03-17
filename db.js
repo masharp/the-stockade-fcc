81e97749035a6fb3adc6862f233fdbdf935141ff
Michael Sharp <msharp.oh@gmail.com>
2016-03-17 19:53:27 -0400

    added more react components, added mongodb calls, cleaned up sockets

--- db.js
+++ db.js
@@ -1,7 +1,64 @@
-/*jshint esnext: true */
-"use strict";
-const dotenv = require("dotenv").config();
+'use strict';
+const dotenv = require('dotenv').config();
 const MONGO_URL = process.env.MONGOLAB_URI;
 
 /* Configure MongoDB */
-const MongoClient = require("mongodb").MongoClient;
+const MongoClient = require('mongodb').MongoClient;
+
+module.exports.getSymbols = function getSymbols() {
+  return new Promise( (resolve, reject) => {
+
+    MongoClient.connect(MONGO_URL, (error, db) => {
+      if(error) reject(error);
+
+      db.collection('symbols', (error, collection) => {
+        if(error) reject(error);
+
+          collection.findOne({ name: 'master'}, (error, item) => {
+            if(error) reject(error);
+
+            db.close();
+            resolve(item);
+          });
+      });
+    });
+  });
+}
+
+module.exports.updateSymbols = function updateSymbols(mode, symbol) {
+  return new Promise((resolve, reject) => {
+
+    MongoClient.connect(MONGO_URL, (error, db) => {
+      if(error) reject(error);
+
+      db.collection('symbols', (error, collection) => {
+        if(error) reject(error);
+
+        switch(mode) {
+          case "ADD":
+            collection.update( { name: 'master' },
+              {
+                $push : { list : symbol }
+              }, (error, result) => {
+                if(error) reject(error);
+                console.log(result);
+                db.close();
+                resolve("Success");
+            });
+            break;
+          case "REMOVE":
+            collection.update( { name: 'master' },
+              {
+                $pull : { list: symbol }
+              }, (error, result) => {
+                if(error) reject(error);
+                console.log(result);
+                db.close();
+                resolve("Success");
+              });
+            break;
+        }
+      })
+    })
+  });
+}
--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -71367,33 +71367,55 @@ module.exports = yeast;
   const ReactDOM = require('react-dom');
   const React = require('react');
   const Request = require('request');
-  const D3 = require('react-d3');
+  const LineChart = require('react-d3').LineChart;
   const Socket = require('socket.io-client')();
-
+  var lineData = [{
+    name: "series1",
+    values: [{ x: 0, y: 20 }, { x: 24, y: 10 }],
+    strokeWidth: 3
+  }, {
+    name: "series2",
+    strokeWidth: 3,
+    values: [{ x: 200, y: 82 }, { x: 302, y: 82 }]
+  }];
   /* ------------------------ React Components -------------------------- */
   /* central controller for the app - contains main UI and user input elements */
   class Controller extends React.Component {
     constructor(props) {
       super(props);
-      this.state = { stockData: {} };
+      this.state = { stockData: [], tempData: lineData };
     }
     componentDidMount() {
-      Socket.on('symbols:initiate', this._stocksUpdated.bind(this));
-      Socket.on('symbols:update', this._stocksUpdated.bind(this));
+      Socket.on('update', this._stocksUpdated.bind(this));
     }
     _stocksUpdated(update) {
-      this.setState({ stockData: update.data });
+      let parsedData = update.split('\n');
+      console.log(parsedData);
+
+      this.setState({ stockData: parsedData });
     }
     handleSymbolSubmit(symbol) {
-      Socket.emit('symbols:added', symbol);
+      Socket.emit('added', symbol);
     }
     handleSymbolRemove(symbol) {
-      Socket.emit('symbols:removed', symbol);
+      Socket.emit('removed', symbol);
     }
     render() {
-      return React.createElement('div', { id: 'main' });
+      return React.createElement('div', { id: 'main' }, React.createElement('div', { id: 'line-chart' }, React.createElement(LineChart, { fill: 'white', title: 'Observed Stock Symbols', width: 1000,
+        data: this.state.tempData })), React.createElement(Stockade, {}));
     }
   }
+
+  class Stockade extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return React.createElement('div', { id: 'stockade' }, React.createElement(Stock, { data: 'Stock' }));
+    }
+  }
+
+  const Stock = props => React.createElement('div', {}, props.data);
 
   Controller.propTypes = {};
 
