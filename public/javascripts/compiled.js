4ec75a96236640e444af358427336a5f4aa14280
Michael Sharp <msharp.oh@gmail.com>
2016-03-17 17:50:28 -0400

    working on web sockets and database

--- public/javascripts/compiled.js
+++ public/javascripts/compiled.js
@@ -6,19 +6,27 @@
   const React = require('react');
   const Request = require('request');
   const D3 = require('react-d3');
-  const IO = require('socket.io-client');
+  const Socket = require('socket.io-client')();
 
-  let socket = IO();
-  socket.on('stock-data', function (message) {
-    console.log("STUFF");
-    console.log(message);
-  });
   /* ------------------------ React Components -------------------------- */
   /* central controller for the app - contains main UI and user input elements */
   class Controller extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {};
+      this.state = { stockData: {} };
+    }
+    componentDidMount() {
+      Socket.on('symbols:initiate', this._stocksUpdated.bind(this));
+      Socket.on('symbols:update', this._stocksUpdated.bind(this));
+    }
+    _stocksUpdated(update) {
+      this.setState({ stockData: update.data });
+    }
+    handleSymbolSubmit(symbol) {
+      Socket.emit('symbols:added', symbol);
+    }
+    handleSymbolRemove(symbol) {
+      Socket.emit('symbols:removed', symbol);
     }
     render() {
       return React.createElement('div', { id: 'main' });
@@ -26,5 +34,6 @@
   }
 
   Controller.propTypes = {};
+
   ReactDOM.render(React.createElement(Controller, {}), document.getElementById('loader'));
 })();
